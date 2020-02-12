5b2af4f4817bb88b4cc14189fef1abab6ec266c4
Thinzar <thinzarnaing19@gmail.com>
2020-02-12 15:20:46 +0630

    renew

--- app.js
+++ app.js
@@ -124,15 +124,7 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
   let response;
   
-  // Checks if the message contains text
-  if (received_message.text == "Hi") {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text":"မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်"
-    }
-  }
-  else if (received_message.text == "Hello") {    
+  if (received_message.text == "Hello") {    
     response = {
       "text":"မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်"
     }

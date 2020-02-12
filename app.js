cbb43cc0b4a8d777279f3c492e733d036e5d916c
Thinzar <thinzarnaing19@gmail.com>
2020-02-12 11:36:59 +0630

    menu

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ function handleMessage(sender_psid, received_message) {
   let response;
   
   // Checks if the message contains text
-  if (received_message.text == "color") {    
+  if (received_message.text == "hi") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
@@ -146,6 +146,20 @@ function handleMessage(sender_psid, received_message) {
     ]
     }
   }
+  {
+  "recipient":{
+    "id":"<PSID>"
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"<TEMPLATE_TYPE>",
+        ...
+      }
+    }
+  }
+}
   else if (received_message.text == "hello") {
     response = {
       "text":'Hello..Min Ga Lar Par'

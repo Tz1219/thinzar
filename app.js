35c9c3b5b72bfa8e0c4f2c8236006c2cad19da2d
Thinzar <thinzarnaing19@gmail.com>
2020-04-28 16:11:59 +0630

    renew

--- app.js
+++ app.js
@@ -166,8 +166,7 @@ function handleMessage(sender_psid, received_message) {
     callSend(sender_psid, response1).then(()=>{
       return callSend(sender_psid, response2);
         });
-  }
-   }  
+  }  
    else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;

e2331bf663eef52de66b4d9dc7fc306f5b7780a3
Thinzar <thinzarnaing19@gmail.com>
2020-04-29 13:58:54 +0630

    renew

--- app.js
+++ app.js
@@ -134,6 +134,19 @@ function handleMessage(sender_psid, received_message) {
       let response2 = {
       "text":"Hi. if you have any questions or concerns, please send them a photo and you will be asked to answer in the near future. Thanks you. If you have delivered your order, please contact us with your phone!",
     }
+       "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"<POSTBACK_PAYLOAD>",
+        "image_url":"http://example.com/img/red.png"
+      },{
+        "content_type":"text",
+        "title":"Green",
+        "payload":"<POSTBACK_PAYLOAD>",
+        "image_url":"http://example.com/img/green.png"
+      }
+    ]
        callSend(sender_psid, response1).then(()=>{
       return callSend(sender_psid, response2);
         });

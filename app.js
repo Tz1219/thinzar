4da51e699e7f8cd04ea0d36ef0ed02b7500801ba
Tz1219 <60253891+Tz1219@users.noreply.github.com>
2020-05-01 13:47:53 +0630

    Update app.js

--- app.js
+++ app.js
@@ -498,7 +498,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!')
+      console.log(body);
     } else {
       console.error("Unable to send message:" + err);
     }

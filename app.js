672bc2bb0573b9852620edaf04cb9b5ddde84d1e
Thinzar <thinzarnaing19@gmail.com>
2020-05-09 14:08:30 +0630

    renew

--- app.js
+++ app.js
@@ -259,7 +259,7 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === 'yes') {
-    response = { "text": "Give your size!(e.g 14,16,18)"}
+    response = { "text": "Give your size!"}
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   }else if (payload === 'getstarted') {
@@ -502,7 +502,7 @@ function handlePostback(sender_psid, received_postback) {
     }
   }
   } else if (payload === 'o') {
-    response = { "text": "Give your size!" }
+    response = { "text": "Give your size!(e.g 14,16,18)" }
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);

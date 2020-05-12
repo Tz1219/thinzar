72adf7fa0ec3f8fb743e13bb9bb9208ef34db63d
Thinzar <thinzarnaing19@gmail.com>
2020-05-12 17:36:33 +0630

    renew

--- app.js
+++ app.js
@@ -207,7 +207,6 @@ function handleMessage(sender_psid, received_message) {
       }
   }  
 
-/*
    else if (received_message.text == "Order") {
       response = {
         "text":'Thanks you! Will you come to shop!',
@@ -223,7 +222,7 @@ function handleMessage(sender_psid, received_message) {
         }
       ]    
       }
-  }*/
+  }
    else if (received_message.text == "Cancle") {
       response = {
         "text":'Thanks!' 

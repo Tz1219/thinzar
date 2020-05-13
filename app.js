ca23c262538386cdda7b135be2fa880f2c799f00
Thinzar <thinzarnaing19@gmail.com>
2020-05-13 18:45:08 +0630

    renew

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ function handleMessage(sender_psid, received_message) {
   }
   else if (received_message.text && reqQuestion.weight == true){
     customerAns.weight == received_message.text;
-    response ={ "text": 'You will receice your order within a week and price will cost 300000.',
+    response ={ "text": 'You will receice your order within a week and price will cost 500000.',
       "quick_replies":[
       {
         "content_type": "text",
